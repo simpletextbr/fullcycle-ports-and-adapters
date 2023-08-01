888f78b2dc695a75e9b600b1b90402158c552d34
Wesley Praca <simpletextbr@gmail.com>
2023-08-01 20:52:23 -0300

    Implementando metodo Save

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -11,6 +11,10 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
 func (p *ProductDb) Get(id string) (application.IProduct, error) {
 	var product application.Product
 	stmt, err := p.db.Prepare("SELECT id, name, price, status FROM products WHERE id = ?")
@@ -25,4 +29,49 @@ func (p *ProductDb) Get(id string) (application.IProduct, error) {
 	}
 
 	return &product, nil
+}
+func (p *ProductDb) Save(product application.IProduct) (application.IProduct, error) {
+	var rows int
+	p.db.QueryRow("SELECT COUNT(id) FROM products WHERE id = ?", product.GetID()).Scan(&rows)
+	if rows == 0 {
+		if _, err := p.create(product); err != nil {
+			return nil, err
+		}
+	} else {
+		if _, err := p.update(product); err != nil {
+			return nil, err
+		}
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) create(product application.IProduct) (application.IProduct, error) {
+	stmt, err := p.db.Prepare("INSERT INTO products(id, name, price, status) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.GetID(), product.GetName(), product.GetPrice(), product.GetStatus())
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.IProduct) (application.IProduct, error) {
+	stmt, err := p.db.Prepare("UPDATE products SET name = ?, price = ?, status = ? WHERE id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.GetName(), product.GetPrice(), product.GetStatus(), product.GetID())
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
 }
--- tests/adapters/db/product_test.go
+++ tests/adapters/db/product_test.go
@@ -3,6 +3,10 @@ package db_test
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/simpletextbr/fullcycle-ports-and-adapters/adapters/db"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
@@ -37,4 +41,16 @@ func createProduct(db *sql.DB) {
 	}
 
 	stmt.Exec("abc", "Product Test", 10.30, "disabled")
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 10.30, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
 }
