7b4d03bc66d9f94172fb1a0aa4dfbadd6433a957
Wesley Praca <simpletextbr@gmail.com>
2023-08-07 14:57:51 -0300

    enable e disable implementado

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -12,13 +12,21 @@ import (
 
 func MakeProductHandler(r *mux.Router, n *negroni.Negroni, service application.IProductService) {
 
-	r.Handle("/products/{id}", n.With(
+	r.Handle("/product/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
 	)).Methods("GET", "OPTIONS")
 
 	r.Handle("/product", n.With(
 		negroni.Wrap(createProduct(service)),
 	)).Methods("POST", "OPTIONS")
+
+	r.Handle("/product/{id}/enable", n.With(
+		negroni.Wrap(enableProduct(service)),
+	)).Methods("PATCH", "OPTIONS")
+
+	r.Handle("/product/{id}/disable", n.With(
+		negroni.Wrap(disableProduct(service)),
+	)).Methods("PATCH", "OPTIONS")
 
 }
 
@@ -62,6 +70,63 @@ func createProduct(service application.IProductService) http.Handler {
 		}
 
 		err = json.NewEncoder(w).Encode(prodcut)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(JsonError(err.Error()))
+			return
+		}
+	})
+
+}
+
+func enableProduct(service application.IProductService) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		product, err := service.GetByID(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		productToEnable, err := service.Enable(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(JsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(productToEnable)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(JsonError(err.Error()))
+			return
+		}
+	})
+}
+
+func disableProduct(service application.IProductService) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		product, err := service.GetByID(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		productToDisable, err := service.Disable(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(JsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(productToDisable)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write(JsonError(err.Error()))
